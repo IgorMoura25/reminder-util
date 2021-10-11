7211e6a3c9bdcb7ee063584a2b3bb4a6d0e5b04c
Igor Moura <igor.moura254@hotmail.com>
2021-10-11 11:55:04 -0300

    Reading connection string from external file

---
+++ UtilTest/TestConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace IgorMoura.Reminder.UtilTest
+{
+    public static class TestConfiguration
+    {
+        public static string ConnectionString
+        {
+            get
+            {
+                var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+                var defaultConnection = config["Data:DefaultConnection:ConnectionString"];
+
+                if (string.IsNullOrEmpty(defaultConnection) || string.IsNullOrWhiteSpace(defaultConnection))
+                {
+                    defaultConnection = config["Data:TestPipelineConnection:ConnectionString"];
+                }
+
+                return defaultConnection;
+            }
+        }
+    }
+}
